4a967c8b7e09a8bd405014dc63a4e6f661f34452
Kathleen Ruan <kruan@andrew.cmu.edu>
2016-12-04 01:16:37 -0500

    random stuff

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,32 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
+
+//adding content to main
+$("#main").append("My name is [name_here].");
+var formattedName = HTMLheaderName.replace("%data%", "Carnegie Mellon University");
+
+//$("#header").prepend(formattedName);
+
+var role="Pittsburgh, Pennsylvania";
+
+
+var formattedRole = HTMLheaderRole.replace("%data%", role);
+$("#header").prepend(formattedRole);
+$("#header").prepend(formattedName);
+
+
+
+var bio = {
+   "name": "Charitard",
+   "role": "pokemon",
+   "contact_info": "mountain range",
+   "pic" : "images/profile_pic.png",
+   "msg": "RAWRRRR!!!!!",
+   "skills": "pokemon battling, flying, ember, quick attack"
+}
+
+$("#main").append(bio);
+
+
+
